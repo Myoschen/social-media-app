ed92a7c1e5a4a5a70f3fe33637965a818dcaa8ce
Myos <myos.chen@gmail.com>
2023-04-28 16:36:24 +0800

    :sparkles: extend chakra theme
    change heading, body fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import '@fontsource/work-sans';
+import '@fontsource/mulish';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './libs/routes';
+import { router } from '@/libs/routes';
+import theme from '@/libs/theme';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <RouterProvider router={router} />
     </ChakraProvider>
   );
